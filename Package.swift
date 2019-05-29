e685ac2875474af3ddf1419f09b545a930b746ce
Ferran Pujol Camins <ferran.pujol@mytaxi.com>
2019-05-29 15:57:35 +0200

    Specify platform in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Time",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v3)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library( name: "Time", targets: ["Time"] )
